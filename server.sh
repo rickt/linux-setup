dd31974e9f217aca8c8f9c59278272688ab84559
rick tait <rickt@rickt.org>
2024-07-30 12:30:30 -0700

    initial checkin

---
+++ server.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# update
+sudo apt update
+
+# install some things
+sudo apt install -y git curl build-essential pkg-config autoconf bison apache2-utils figlet pwgen atop htop nmon bmon telnet ncat iptraf-ng bwm-ng tmux nmap sysstat net-tools nmap keychain whois python3-venv snapd vnstat vim-nox neofetch fzf vim prettyping jq fping tree
+
+# set timezone
+sudo timedatectl set-timezone America/Los_Angeles
+
+# EOF
