23d5f50056614f0518ba0f00a6320c8323a8785f
rick tait <rickt@rickt.org>
2024-09-19 12:37:37 -0700

    added ms code

--- server.sh
+++ server.sh
@@ -13,6 +13,13 @@ sudo apt install -y ffmpeg yt-dlp imagemagick network-manager-openconnect-gnome
 # gnome 
 sudo apt-get install gnome-browser-connector
 
+# MS Code
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+sudo apt update && sudo aot install code
+
 # ultralauncher
 sudo add-apt-repository universe -y && sudo add-apt-repository ppa:agornostal/ulauncher -y && sudo apt update && sudo apt -y install ulauncher
 
