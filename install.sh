06f69524b352bab28d9567c5a6fd840a2d5f3632
rick tait <rick.tait@hypermediasystems.com>
2024-10-02 07:40:18 -0700

    added some -qq's to some apt's

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ chmod -R 700 $HOME/.ssh
 
 # install the basics that all boxes need
 echo "*** installing the basics ***"
-sudo apt install -y git curl build-essential apache2-utils figlet pwgen atop btop htop \
+sudo apt install -y -qq git curl build-essential apache2-utils figlet pwgen atop btop htop \
      bmon nmon telnet ncat iptraf-ng bwm-ng nmap sysstat net-tools keychain whois \
      python3-venv vnstat vim-nox fzf vim jq fping tree lsof bind9-dnsutils
 
@@ -34,7 +34,7 @@ sudo apt install -y git curl build-essential apache2-utils figlet pwgen atop bto
 echo "*** qemu ***"
 BOXTYPE=$(systemd-detect-virt)
 if [ "$BOXTYPE" == "kvm" ]; then
-	sudo apt install -y qemu-guest-agent
+	sudo apt install -y -qq qemu-guest-agent
 fi
 
 # install gui stuff (laptop/workstation that i'm logged into gnome on)
@@ -48,34 +48,34 @@ if $RUNNING_GNOME; then
 	cd -
 
 	# install vpn stuff
-	sudo apt install -y network-manager-openconnect-gnome 
+	sudo apt install -y -qq network-manager-openconnect-gnome 
 
 	# install gnome
-	sudo apt install -y gnome-browser-connector gnome-tweaks
+	sudo apt install -y -qq gnome-browser-connector gnome-tweaks
 
 	# install tabby terminal
 	curl -s https://packagecloud.io/install/repositories/eugeny/tabby/script.deb.sh | sudo bash
-	sudo apt install -y tabby-terminal
+	sudo apt install -y -qq tabby-terminal
 
 	# install tailscale
 	curl -fsSL https://tailscale.com/install.sh | sh
 
 	# install ultralauncher
-	sudo add-apt-repository universe -y && sudo add-apt-repository -y ppa:agornostal/ulauncher -y && sudo apt update && sudo apt -y install ulauncher
+	sudo add-apt-repository universe -y && sudo add-apt-repository -y ppa:agornostal/ulauncher -y && sudo apt update && sudo apt -y -qq install ulauncher
 
 	# install Code
 	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 	echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
 	rm -f packages.microsoft.gpg
-	sudo apt update && sudo apt install code
+	sudo apt update && sudo apt install -qq code
 
 	# install OG docker
 	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 	sudo chmod a+r /etc/apt/keyrings/docker.asc
 	echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 	sudo apt update
-	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	sudo apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 	sudo usermod -aG docker ${USER}
 	echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
 
@@ -98,10 +98,10 @@ if $RUNNING_GNOME; then
 	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
 	sudo apt update &&
-	sudo apt install gh -y
+	sudo apt install -y -qq gh
 
 	# install various stuff
-	sudo apt install -y ffmpeg yt-dlp
+	sudo apt install -y -qq ffmpeg yt-dlp
 fi
 
 # EOF
